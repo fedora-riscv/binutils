fedora-riscv/binutils

commit 735a883933ee358933145522cbe019a0ee7cf32e

Disallow the creation of shared object that use text relocations.  (#2428281)

binutils.spec
@@ -7,7 +7,7 @@ Name: binutils%{?_with_debug:-debug}
 # The variable %%{source} (see below) should be set to indicate which of these
 # origins is being used.
 Version: 2.45.50
-Release: 14%{?dist}
+Release: 15%{?dist}
 License: GPL-3.0-or-later AND (GPL-3.0-or-later WITH Bison-exception-2.2) AND (LGPL-2.0-or-later WITH GCC-exception-2.0) AND BSD-3-Clause AND GFDL-1.3-or-later AND GPL-2.0-or-later AND LGPL-2.1-or-later AND LGPL-2.0-or-later
 URL: https://sourceware.org/binutils
 
@@ -94,6 +94,14 @@ URL: https://sourceware.org/binutils
 %else
 %define enable_separate_code 0
 %endif
+
+# Enable the creation of relocations against the contents of read-only
+# sections (such as .text).  This is a security vulnerability, so it is
+# disabled here by default.
+# Note - the upstream GNU Binutils sources enable the generation of this
+# kind of relocation by default, so this is a difference between Ferdora
+# and upstream.
+%define enable_textrel 0
 
 # Indicate where the sources come from.
 #
@@ -330,6 +338,10 @@ Patch18: binutils-gold-i386-gnu-property-notes.patch
 # Lifetime: Fixed in 2.46 (maybe)
 Patch19: binutils-gold-empty-dwp.patch
 %endif
+
+# Purpose:  Fix ld testsuite failures when enable_textrel is set.
+# Lifetime: Permanent.
+Patch20: binutils-ld-default-z-text.patch
 
 #----------------------------------------------------------------------------
 
@@ -782,6 +794,12 @@ compute_global_configuration()
     CARGS="$CARGS --enable-threads=yes"
 %else
     CARGS="$CARGS --enable-threads=no"
+%endif
+
+%if %{enable_textrel}
+    CARGS="$CARGS --enable-textrel-check=no"
+%else
+    CARGS="$CARGS --enable-textrel-check=error"
 %endif
 
 %if "%{source}" != "official-release"
@@ -1485,6 +1503,9 @@ exit 0
 
 #----------------------------------------------------------------------------
 %changelog
+* Tue Jan 13 2026 Nick Clifton <nickc@redhat.com> - 2.45.50-15
+- Disallow the creation of shared object that use text relocations.  (#2428281)
+
 * Fri Jan 09 2026 Nick Clifton <nickc@redhat.com> - 2.45.50-14
 - Fix Risc-V related test failures caused by -12 revision. 
 

binutils-ld-default-z-text.patch
@@ -1,0 +1,52 @@
+diff -rup binutils.orig/bfd/elflink.c binutils-with-gold-2.45.50-be970c68891/bfd/elflink.c
+--- binutils.orig/bfd/elflink.c	2026-01-14 09:21:14.835847967 +0000
++++ binutils-with-gold-2.45.50-be970c68891/bfd/elflink.c	2026-01-14 09:23:48.780495205 +0000
+@@ -13786,7 +13786,7 @@ _bfd_elf_final_link (bfd *abfd, struct b
+ 		{
+ 		  if (info->textrel_check == textrel_check_error)
+ 		    info->callbacks->einfo
+-		      (_("%P%X: read-only segment has dynamic relocations\n"));
++		      (_("%P%X: error: read-only segment has dynamic relocations\n"));
+ 		  else if (bfd_link_dll (info))
+ 		    info->callbacks->einfo
+ 		      (_("%P: warning: creating DT_TEXTREL in a shared object\n"));
+diff -rup binutils.orig/ld/testsuite/ld-aarch64/dt-memtag-mode.d binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/dt-memtag-mode.d
+--- binutils.orig/ld/testsuite/ld-aarch64/dt-memtag-mode.d	2026-01-14 09:21:15.248851537 +0000
++++ binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/dt-memtag-mode.d	2026-01-14 09:22:24.603416270 +0000
+@@ -1,5 +1,5 @@
+ #source: dt-memtag.s
+-#ld: -shared -z memtag-mode=async
++#ld: -shared -z memtag-mode=async -z notext
+ #readelf: -d
+ 
+ #...
+diff -rup binutils.orig/ld/testsuite/ld-aarch64/dt-memtag-stack.d binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/dt-memtag-stack.d
+--- binutils.orig/ld/testsuite/ld-aarch64/dt-memtag-stack.d	2026-01-14 09:21:15.248851537 +0000
++++ binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/dt-memtag-stack.d	2026-01-14 09:24:19.594780414 +0000
+@@ -1,5 +1,5 @@
+ #source: dt-memtag.s
+-#ld: -shared -z memtag-stack
++#ld: -shared -z memtag-stack -z notext
+ #readelf: -d
+ 
+ #...
+diff -rup binutils.orig/ld/testsuite/ld-aarch64/relr-text-pie.d binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/relr-text-pie.d
+--- binutils.orig/ld/testsuite/ld-aarch64/relr-text-pie.d	2026-01-14 09:21:15.246851520 +0000
++++ binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/relr-text-pie.d	2026-01-14 09:24:45.363485698 +0000
+@@ -1,5 +1,5 @@
+ #source: relr-text.s
+-#ld: -pie -z pack-relative-relocs -T relocs.ld
++#ld: -pie -z pack-relative-relocs -T relocs.ld -z notext
+ #readelf: -drW
+ 
+ #...
+diff -rup binutils.orig/ld/testsuite/ld-aarch64/relr-text-shared.d binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/relr-text-shared.d
+--- binutils.orig/ld/testsuite/ld-aarch64/relr-text-shared.d	2026-01-14 09:21:15.246851520 +0000
++++ binutils-with-gold-2.45.50-be970c68891/ld/testsuite/ld-aarch64/relr-text-shared.d	2026-01-14 09:25:06.651664713 +0000
+@@ -1,5 +1,5 @@
+ #source: relr-text.s
+-#ld: -shared -z pack-relative-relocs -T relocs.ld
++#ld: -shared -z pack-relative-relocs -T relocs.ld -z notext
+ #readelf: -drW
+ 
+ #...

commit aa57e62e0884cdf03cd314eb94c2b777dce4d02c

Fix Risc-V linker testsuite failures caused by #2428281.

binutils-riscv-testsuite-fixes.patch
@@ -217,28 +217,6 @@ diff -rup binutils-with-gold-2.45.50-beab972c07d,orig/binutils/testsuite/binutil
  strip_test_with_saving_a_symbol
  
  # Test stripping an archive.
-@@ -1186,6 +1194,10 @@ switch [copy_setup] {
- 					 "$OBJCOPY" "$OBJCOPYFLAGS" "$test5"
- 	if [is_elf_format] {
- 	    keep_debug_symbols_and_check_links "$STRIP" "--only-keep-debug $STRIPFLAGS" "$test6"
-+
-+	    # The RISC-V target compiles with annotation enabled and these symbols remain after stripping.
-+	    setup_xfail riscv*-*-* 
-+	    
- 	    keep_debug_symbols_for_elf_relocatable "$STRIP" "--only-keep-debug $STRIPFLAGS" "$test7"
- 	}
-     }
-@@ -1197,6 +1209,10 @@ switch [copy_setup] {
- 					 "$OBJCOPY" "$OBJCOPYFLAGS" "$test5"
- 	if [is_elf_format] {
- 	    keep_debug_symbols_and_check_links "$STRIP" "--only-keep-debug $STRIPFLAGS" "$test6"
-+
-+	    # The RISC-V target compiles with annotation enabled and these symbols remain after stripping.
-+	    setup_xfail riscv*-*-* 
-+
- 	    keep_debug_symbols_for_elf_relocatable "$STRIP" "--only-keep-debug $STRIPFLAGS" "$test7"
- 	}
-     }
 @@ -1503,7 +1519,7 @@ proc objcopy_test_without_global_symbol
  # in object files, so they will fail this test.
  setup_xfail aarch64*-*-* arm*-*-*

binutils-testsuite-fixes.patch
@@ -1081,3 +1081,69 @@ diff -rup bin.orig/ld/testsuite/ld-x86-64/sframe-pltgot-2.d binutils-2.43.50-55e
  
  # Test objdump --map-global-vars on file containing dwarf-2 encoding information.
  
+diff -rup binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pic.d binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pic.d
+--- binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pic.d	2026-03-20 13:02:05.468700958 +0000
++++ binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pic.d	2026-03-20 13:05:31.155200130 +0000
+@@ -1,6 +1,6 @@
+ #source: data-reloc.s
+ #as: -march=rv32i -mabi=ilp32 -defsym __abs__=1 -defsym __addr__=1 -defsym __undef__=1
+-#ld: -m[riscv_choose_ilp32_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared
++#ld: -m[riscv_choose_ilp32_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared -z notext
+ #objdump: -dR
+ 
+ .*:[ 	]+file format .*
+diff -rup binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pie.d binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pie.d
+--- binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pie.d	2026-03-20 13:02:05.468700958 +0000
++++ binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv32-pie.d	2026-03-20 13:05:52.846252771 +0000
+@@ -1,6 +1,6 @@
+ #source: data-reloc.s
+ #as: -march=rv32i -mabi=ilp32 -defsym __abs__=1 -defsym __addr__=1
+-#ld: -m[riscv_choose_ilp32_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -pie
++#ld: -m[riscv_choose_ilp32_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -pie -z notext
+ #objdump: -dR
+ 
+ .*:[ 	]+file format .*
+diff -rup binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv32-symbolic.d binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv32-symbolic.d
+--- binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv32-symbolic.d	2026-03-20 13:02:05.468700958 +0000
++++ binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv32-symbolic.d	2026-03-20 13:06:01.427273593 +0000
+@@ -1,6 +1,6 @@
+ #source: data-reloc.s
+ #as: -march=rv32i -mabi=ilp32 -defsym __abs__=1 -defsym __addr__=1 -defsym __undef__=1
+-#ld: -m[riscv_choose_ilp32_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared -Bsymbolic
++#ld: -m[riscv_choose_ilp32_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared -Bsymbolic -z notext
+ #objdump: -dR
+ 
+ .*:[ 	]+file format .*
+diff -rup binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pic.d binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pic.d
+--- binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pic.d	2026-03-20 13:02:05.468700958 +0000
++++ binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pic.d	2026-03-20 13:04:07.601997355 +0000
+@@ -1,6 +1,6 @@
+ #source: data-reloc.s
+ #as: -march=rv64i -mabi=lp64 -defsym __64_bit__=1 -defsym __abs__=1 -defsym __addr__=1 -defsym __undef__=1
+-#ld: -m[riscv_choose_lp64_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared
++#ld: -m[riscv_choose_lp64_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared -z notext
+ #objdump: -dR
+ 
+ .*:[ 	]+file format .*
+diff -rup binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pie.d binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pie.d
+--- binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pie.d	2026-03-20 13:02:05.468700958 +0000
++++ binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv64-pie.d	2026-03-20 13:04:57.106117494 +0000
+@@ -1,6 +1,6 @@
+ #source: data-reloc.s
+ #as: -march=rv64i -mabi=lp64 -defsym __64_bit__=1 -defsym __abs__=1 -defsym __addr__=1
+-#ld: -m[riscv_choose_lp64_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -pie
++#ld: -m[riscv_choose_lp64_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -pie -z notext
+ #objdump: -dR
+ 
+ .*:[ 	]+file format .*
+diff -rup binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv64-symbolic.d binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv64-symbolic.d
+--- binutils.orig/ld/testsuite/ld-riscv-elf/data-reloc-rv64-symbolic.d	2026-03-20 13:02:05.468700958 +0000
++++ binutils-with-gold-2.46/ld/testsuite/ld-riscv-elf/data-reloc-rv64-symbolic.d	2026-03-20 13:05:14.650160073 +0000
+@@ -1,6 +1,6 @@
+ #source: data-reloc.s
+ #as: -march=rv64i -mabi=lp64 -defsym __64_bit__=1 -defsym __abs__=1 -defsym __addr__=1 -defsym __undef__=1
+-#ld: -m[riscv_choose_lp64_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared -Bsymbolic
++#ld: -m[riscv_choose_lp64_emul] -Ttext 0x8000 --defsym _start=0x0 --defsym abs=0x100 --defsym abs_local=0x200 -shared -Bsymbolic -z notext
+ #objdump: -dR
+ 
+ .*:[ 	]+file format .*

binutils.spec
@@ -7,7 +7,7 @@ Name: binutils%{?_with_debug:-debug}
 # The variable %%{source} (see below) should be set to indicate which of these
 # origins is being used.
 Version: 2.46.50
-Release: 3%{?dist}
+Release: 4%{?dist}
 License: GPL-3.0-or-later AND (GPL-3.0-or-later WITH Bison-exception-2.2) AND (LGPL-2.0-or-later WITH GCC-exception-2.0) AND BSD-3-Clause AND GFDL-1.3-or-later AND GPL-2.0-or-later AND LGPL-2.1-or-later AND LGPL-2.0-or-later
 URL: https://sourceware.org/binutils
 
@@ -1492,6 +1492,9 @@ exit 0
 
 #----------------------------------------------------------------------------
 %changelog
+* Fri Mar 20 2026 Nick Clifton <nickc@redhat.com> - 2.46.50-4
+- Fix Risc-V linker testsuite failures caused by #2428281.
+
 * Mon Mar 16 2026 Nick Clifton <nickc@redhat.com> - 2.46.50-3
 - Rebase to commit d712202846b.
 